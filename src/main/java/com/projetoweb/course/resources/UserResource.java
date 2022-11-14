215f5db1b11a8372da7563e74a43c57286e79c97
Tiago Lima <tiagoo.lima15@gmail.com>
2022-11-14 15:10:19 -0300

    User entity and resouce

---
+++ src/main/java/com/projetoweb/course/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.projetoweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetoweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		
+		User u = new User(1L, "Tiago", "tiago@gmail.com", "123456789", "12345");		
+		return ResponseEntity.ok().body(u);
+		
+	}
+		
+}
