e34ac2a7000117fb81be88c26b5b4d6e6f37b3cb
Tiago Lima <tiagoo.lima15@gmail.com>
2022-11-15 12:07:30 -0300

    implementing order entity

--- src/main/java/com/projetoweb/course/entities/User.java
+++ src/main/java/com/projetoweb/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.projetoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String Password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -74,6 +80,11 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		Password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -91,7 +102,4 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-
 }
