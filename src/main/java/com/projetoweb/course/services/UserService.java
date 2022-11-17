60d9e7adedbb681a9f93c8567eaa21ca6bc6e5d5
Tiago Lima <tiagoo.lima15@gmail.com>
2022-11-17 11:26:49 -0300

    Exception handling - update

--- src/main/java/com/projetoweb/course/services/UserService.java
+++ src/main/java/com/projetoweb/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.projetoweb.course.repositories.UserRepository;
 import com.projetoweb.course.services.exceptions.DatabaseException;
 import com.projetoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -45,9 +47,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = userRepository.getReferenceById(id);
-		updateData(entity, obj);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getReferenceById(id);
+			updateData(entity, obj);
+			return userRepository.save(entity);
+		}catch (EntityNotFoundException e){
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void updateData(User entity, User obj) {
